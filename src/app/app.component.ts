7c9aae721943018f7b5a430cbfe2646ebc7e23e9
Nathan <nathan.tomines00@gmail.com>
2023-11-16 14:33:58 +0100

    aggiunto il counter e il play del counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pomodoro-timer';
+
+  counterTime = { mins: 25, secs: 0 };
+
+  constructor() {}
+
+  startTimer(event : any) {
+    event.target.disabled = true;
+    let intervalID = setInterval(() => {
+      if (this.counterTime.mins == 0 && this.counterTime.secs == 0)
+        clearInterval(intervalID);
+
+      if(this.counterTime.secs == 0){
+        this.counterTime.mins -= 1
+        this.counterTime.secs = 59
+      }else
+        this.counterTime.secs -=1
+    }, 1000);
+  }
 }
