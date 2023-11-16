8fc0961d9d460806bc4d3401ff7610abd8ade4c5
Nathan <nathan.tomines00@gmail.com>
2023-11-16 16:17:11 +0100

    aggiunto bottoni pausa,resume e reset con le funzionalità

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,43 @@ export class AppComponent {
   title = 'pomodoro-timer';
 
   counterTime = { mins: 25, secs: 0 };
-
+  timerStarting = false
+  intervalID!: any
+  paused = false
   constructor() {}
 
   startTimer(event : any) {
-    event.target.disabled = true;
-    let intervalID = setInterval(() => {
-      if (this.counterTime.mins == 0 && this.counterTime.secs == 0)
-        clearInterval(intervalID);
+    this.timerStarting = true
+    this.intervalID = this.doCountdown()
+  }
 
+  doCountdown(){
+    return setInterval(() => {
+      if (this.counterTime.mins == 0 && this.counterTime.secs == 0){
+        this.timerStarting = false;
+        clearInterval(this.intervalID);
+      }
       if(this.counterTime.secs == 0){
         this.counterTime.mins -= 1
         this.counterTime.secs = 59
       }else
         this.counterTime.secs -=1
     }, 1000);
+  }
+
+  pauseTimer(){
+    this.paused = true;
+    clearInterval(this.intervalID);
+  }
+
+  resumeTimer(){
+    this.paused = false;
+    this.intervalID = this.doCountdown()
+  }
+
+  resetTimer(){
+    this.counterTime = { mins: 25, secs: 0 };
+    this.timerStarting = false
+    this.paused = false
   }
 }
